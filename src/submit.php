aa38034b0ebf8b9a60854e785445827d9bcefbc8
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2024-03-29 10:21:09 +0300

    Update submit.php

--- src/submit.php
+++ src/submit.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+<style>
+    @import "styles/styles.css";
+</style>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Submission Result</title>
 </head>
 <body>
+<div class="form-container">
 <h2>Form Submission Result</h2>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     foreach ($_POST as $key => $value) {
+        $upKey = ucfirst($key);
         if (!is_array($value)) {
-            echo "<p>$key: $value</p>";
+            echo "<p style='margin: 0;'><strong>$upKey:</strong> $value</p>";
         } else {
-            echo "<p>$key: " . implode(", ", $value) . "</p>";
+            echo "<p style='margin: 0;'><strong>$upKey:</strong> " . implode(", ", $value) . "</p>";
         }
     }
     if(isset($_FILES['file'])){
@@ -22,11 +27,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $file_size = $_FILES['file']['size'];
         $file_type = $_FILES['file']['type'];
         move_uploaded_file($file_tmp,"uploads/".$file_name);
-        echo "<p>File: <a href='uploads/".$file_name."' target='_blank'>".$file_name."</a></p>";
+        echo "<p style='margin: 0;'><strong>File:</strong> <a href='uploads/".$file_name."' target='_blank'>".$file_name."</a></p>";
     }
 } else {
     echo "<p>No data submitted</p>";
 }
 ?>
+</div>
 </body>
 </html>
